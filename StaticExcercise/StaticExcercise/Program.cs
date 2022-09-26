a8a5b6180833ad4dfe9c767bcb36496400e675a4
Gary Li <garyli0802@yahoo.com>
2022-09-26 11:39:16 -0700

    initial commit

---
+++ StaticExcercise/StaticExcercise/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using StaticExcercise;
+
+
+namespace StaticExcercise
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+           var celsius =TempConverter.FahrenheitToCelsius(90);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"celsius : {celsius}");
+            Console.WriteLine($"fahrenheit : {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExcercise/StaticExcercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExcercise
+{
+    public class TempConverter
+    {
+       public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            
+           var celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+            
+
+        }
+       public static double CelsiusToFahrenheit(double celsius)
+        {
+            var fahrenheit = (celsius * 9) / 5 + 32;
+            return fahrenheit;
+        }
+    }
+}
